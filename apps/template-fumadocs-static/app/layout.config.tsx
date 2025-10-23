b8757ee399a2629f4308bba10d88876285e18d9e
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2025-10-23 10:33:42 +0200

    init

---
+++ apps/template-fumadocs-static/app/layout.config.tsx
@@ -0,0 +1,31 @@
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
+import config from "@/content/config.json";
+
+/**
+ * Shared layout configurations
+ *
+ * you can customise layouts individually from:
+ * Home Layout: app/(home)/layout.tsx
+ * Docs Layout: app/docs/layout.tsx
+ */
+export const baseOptions: BaseLayoutProps = {
+  nav: {
+    title: (
+      <>
+        {config.nav.image && (
+          <Image
+            src={config.nav.image}
+            alt={config.nav.title}
+            width={24}
+            height={24}
+            className="mr-2"
+          />
+        )}
+        {config.nav.title || "config.nav.title"}
+      </>
+    ),
+  },
+  // see https://fumadocs.dev/docs/ui/navigation/links
+  links: [],
+};
---
+++ apps/template-fumadocs-static/app/layout.tsx
@@ -0,0 +1,25 @@
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+import SearchDialog from "@/app/components/search";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <body className="flex flex-col min-h-screen">
+        <RootProvider
+          search={{
+            SearchDialog,
+          }}
+        >
+          {children}
+        </RootProvider>
+      </body>
+    </html>
+  );
+}
